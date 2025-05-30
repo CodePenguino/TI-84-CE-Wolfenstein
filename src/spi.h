76e443a9ab5d013ccfc916e2deee75c0c5d8fde4
CodePenguino <agrasrodrigo@gmail.com>
2025-05-30 19:29:34 -0400

    Comment correction

--- src/spi.h
+++ src/spi.h
@@ -8,8 +8,7 @@
 
 // The SPI is a piece of hardware that controls how things are displayed on
 // the screen. There currently aren't any C functions to control it on the
-// TI-84 Plus CE Toolchain (as of May 2024), so I have to roll my own with this
-// one...
+// TI-84 Plus CE Toolchain, so I have to partially roll my own with this one...
 
 // Most of the code is modified from calc86maniac's source code for tiboyce
 // https://github.com/calc84maniac/tiboyce/blob/cc5a1fac6fc4840e1483fc6a38c48aff2143549a/setup.asm#L4245
